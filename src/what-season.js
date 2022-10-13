536cfc78ac8458b316321086baf50aa8c0e8cc01
Roman Pchelkin <epos.91@mail.ru>
2022-10-13 12:38:55 +0300

    upd: get season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,9 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(seasonDate = 'empty') {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (seasonDate === 'empty') {
+    return 'Unable to determine the time of year!'
+  }
+  if (seasonDate instanceof Date) {
+    switch (month) {
+      case 0:
+      case 1:
+      case 11:
+        return 'winter';
+      case 2:
+      case 3:
+      case 4:
+        return 'spring';
+      case 5:
+      case 6:
+      case 7:
+        return 'summer'
+      case 8:
+      case 9:
+      case 10:
+        return 'autumn'
+    }
+  }
+  throw new Error('Invalid date!')
 }
 
 module.exports = {
